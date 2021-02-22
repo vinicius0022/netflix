5d469f39debeecf42263716aecc0fccd450276ee
svntxz <viniciussantos1314@gmail.com>
2021-02-22 12:58:17 -0300

    header

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,29 @@ import React from 'react'
 import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
 import { FaqsContainer } from '../containers/faqs'
+import { HeaderContainer } from '../containers/header'
+import { OptForm, Feature } from '../components'
 
 export default function App() {
     return (
         <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Filmes, séries e muito mais. Sem limites.
+</Feature.Title>
+                    <Feature.SubTitle>Assista onde quiser. Cancele quando quiser.
+</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Text>
+                            Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.
+                        </OptForm.Text>
+                        <OptForm.Break />
+                        <OptForm.Input placeholder="E-mail" />
+                        <OptForm.Button>Vamos lá</OptForm.Button>
+                    </OptForm>
+                </Feature>
+                
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
