bbfd7a775d7d445a04bc33e5647fded26f8ca261
svntxz <viniciussantos1314@gmail.com>
2021-03-01 14:08:22 -0300

    hook use-content

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useContent } from '../hooks'
 
 export default function Browse() {
+    const { series } = useContent('series')
+    const { films } = useContent('films')
     return <p>Browse!</p>
 }
