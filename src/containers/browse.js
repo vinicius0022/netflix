747ae9909c2ce299eec9e9338b174d364d7068f5
svntxz <viniciussantos1314@gmail.com>
2021-03-01 17:37:42 -0300

    Profile and Spinner Components

---
+++ src/containers/browse.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { SelectProfileContainer } from './profiles'
+import { FirebaseContext } from '../context/firebase'
+import { Header, Loading } from '../components'
+
+export function BrowseContainer({ slides }) {
+    const [profile, setProfile] = useState({})
+    const [loading, setLoading] = useState(true)
+    const { firebase } = useContext(FirebaseContext)
+    const user = firebase.auth().currentUser || {}
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [profile.displayName])
+
+    return profile.displayName ? (
+        <>
+        {loading ? (<Loading src={user.photoURL} />) : (<Loading.ReleaseBody />)}
+        <Header src="joker1"><p>Olá!</p></Header>
+        </>
+    ) : (<SelectProfileContainer user={user} setProfile={setProfile} />)
+}
--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react'
 import { FirebaseContext } from '../context/firebase'
 
 export default function useContent(target) {
-    const [content, setContent] = useContext([])
+    const [content, setContent] = useState([])
     const{ firebase } = useContext(FirebaseContext)
 
     useEffect(() => {
