4f4e87d82cf856d5b2804535c04cc241f76f07f5
svntxz <viniciussantos1314@gmail.com>
2021-03-01 13:25:39 -0300

    helper auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import { Home, Browse, Signin, Signup } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 export default function App() {
+    const user={ name:'bla'}
     return (
         <Router>
-            <Route exact path={ROUTES.HOME}>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
                 <Home />
-            </Route>
-            <Route exact path={ROUTES.SIGN_IN}>
+            </IsUserRedirect>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
                 <Signin />
-            </Route>
-            <Route exact path={ROUTES.SIGN_UP}>
+            </IsUserRedirect> 
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
                 <Signup />
-            </Route>
-            <Route exact path={ROUTES.BROWSE}>
+            </IsUserRedirect>
+            <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
                 <Browse />
-            </Route>
+            </ProtectedRoute>   
         </Router>
     );
 }
