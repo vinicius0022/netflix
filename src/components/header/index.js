b8083a9fe422b01c3497f888a0b44b7b9dd4f666
svntxz <viniciussantos1314@gmail.com>
2021-03-04 06:12:58 -0300

    Browse Header Profile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Background, Container, Logo, ButtonLink } from './styles/header'
+import { Background, Container, Group, Dropdown, Profile, Picture, Logo, ButtonLink, Feature, FeatureCallOut ,Text, Link } from './styles/header'
 
 export default function Header({ bg= true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>
+}
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps}) {
+    return <Profile {...restProps}>{children}</Profile>
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />
 }
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
 import { Header, Loading } from '../components'
+import * as ROUTES from '../constants/routes'
+import logo from '../logo.svg'
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({})
@@ -18,7 +20,32 @@ export function BrowseContainer({ slides }) {
     return profile.displayName ? (
         <>
         {loading ? (<Loading src={user.photoURL} />) : (<Loading.ReleaseBody />)}
-        <Header src="joker1"><p>Olá!</p></Header>
+        <Header src="joker1">
+            <Header.Frame>
+                <Header.Group>
+                    <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+                    <Header.TextLink>Series</Header.TextLink>
+                    <Header.TextLink>Filmes</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                    <Header.Profile>
+                        <Header.Picture src={user.photoURL} />
+                        <Header.Dropdown>
+                            <Header.Group>
+                                <Header.Picture src={user.photoURL} />
+                                <Header.TextLink>{user.displayName}</Header.TextLink>
+                            </Header.Group>
+                        </Header.Dropdown>
+                    </Header.Profile>
+                </Header.Group>                
+            </Header.Frame>
+            <Header.Feature>
+                <Header.FeatureCallOut>Assista Coringa Agora</Header.FeatureCallOut>
+                <Header.Text>
+                    Isolado, intimidado e desconsiderado pela sociedade, o fracassado comediante Arthur Fleck inicia seu caminho como uma mente criminosa após assassinar três homens em pleno metrô. Sua ação inicia um movimento popular contra a elite de Gotham City, da qual Thomas Wayne é seu maior representante.
+                </Header.Text>
+            </Header.Feature>
+        </Header>
         </>
     ) : (<SelectProfileContainer user={user} setProfile={setProfile} />)
 }
