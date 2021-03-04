6dedc8066a00c93bc5246336c2818aa55c6cd792
svntxz <viniciussantos1314@gmail.com>
2021-03-04 10:18:25 -0300

    Player Component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FooterContainer } from './footer'
 import { FirebaseContext } from '../context/firebase'
-import { Card, Header, Loading } from '../components'
+import { Card, Header, Loading, Player } from '../components'
 import * as ROUTES from '../constants/routes'
 import logo from '../logo.svg'
 
@@ -76,9 +76,12 @@ export function BrowseContainer({ slides }) {
                             </Card.Item>
                         ))}
                     </Card.Entities>
-                    {<Card.Feature category={category}>
-                        
-                    </Card.Feature>}
+                    <Card.Feature category={category}>
+                        <Player>
+                            <Player.Button />
+                            <Player.Video src="/videos/bunny.mp4" />
+                        </Player>
+                    </Card.Feature>
                 </Card>
             ))}
         </Card.Group>
